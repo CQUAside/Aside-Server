49b7d13efebf28020e2913b7cca39829f8d218de
A Shuai <ls1110924@gmail.com>
2016-03-30 11:38:57 +0800

    new add junit testcase framework.

---
+++ test/com/round/aside/test/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * 单元测试包。
+ * <P>
+ * 所有的单元测试类都应放到这个包中，且该包已经对除当前文件之外的所有文件提供了忽略，
+ * 避免测试用例文件提交到远程Git repository。
+ * 
+ */
+/**
+ * @author A Shuai
+ * @date 2016-3-30
+ *
+ */
+package com.round.aside.test;

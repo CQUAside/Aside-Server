88cb8b91d7b3a597787ecd254cfdf646975aba0b
A Shuai <ls1110924@gmail.com>
2016-06-18 00:07:40 +0800

    complete imageio module comments.

--- src/com/round/aside/server/module/imageio/IImageIO.java
+++ src/com/round/aside/server/module/imageio/IImageIO.java
@@ -6,6 +6,8 @@ import com.round.aside.server.bean.statuscode.ReadImgStatusCodeBean;
 import com.round.aside.server.bean.statuscode.StatusCodeBean;
 import com.round.aside.server.module.IModule;
 
+import static com.round.aside.server.constant.StatusCode.*;
+
 /**
  * 图片存储、读取、剪裁模块超级接口
  * 
@@ -22,7 +24,9 @@ public interface IImageIO extends IModule {
      *            输入图片数据流
      * @param imgPath
      *            待写入的图片路径
-     * @return 结果状态码
+     * @return 结果状态码，合法的结果值分别有：{@link #S1000}成功；{@link #ER5001}形参非法；
+     *         {@link #R6015}目的图片文件目录创建失败；{@link #EX2031}目的图片文件异常；
+     *         {@link #EX2032}文件IO异常。
      */
     public StatusCodeBean writeImg(InputStream mInput, String imgPath);
 
@@ -31,7 +35,8 @@ public interface IImageIO extends IModule {
      * 
      * @param imgpath
      *            图片路径
-     * @return 图片读取结果状态码数据bean
+     * @return 图片读取结果状态码数据bean，合法的状态码分别有：{@link #S1000}成功；{@link #ER5001}图片路径为空；
+     *         {@link #EX2031}源图片不存在；{@link #EX2032}原图读取异常。
      */
     public ReadImgStatusCodeBean readImg(String imgpath);
 
@@ -50,7 +55,9 @@ public interface IImageIO extends IModule {
      *            裁切图片高度
      * @param mClipImgType
      *            裁切图保存类型
-     * @return 结果状态码
+     * @return 结果状态码，合法的状态码分别有：{@link #S1000}成功；{@link #ER5001}形参非法；
+     *         {@link #R6014}源图不存在或读取失败；{@link #R6015}裁剪图目录创建失败； {@link #EX2032}
+     *         裁剪图写入异常。
      */
     public StatusCodeBean clipImg(String mOriImgPath, String mClipImgPath,
             String mExtension, int mMaxWidth, int mMaxHeight,
@@ -71,7 +78,9 @@ public interface IImageIO extends IModule {
      *            缩略图最大高度
      * @param mThumbImgType
      *            缩略图的保存类型
-     * @return 结果状态码
+     * @return 结果状态码，合法的状态码分别有：{@link #S1000}成功；{@link #ER5001}形参非法；
+     *         {@link #R6014}源图不存在或读取失败；{@link #R6015}缩略图目录创建失败； {@link #EX2032}
+     *         缩略图写入异常。
      */
     public StatusCodeBean zoomImg(String mOriImgPath, String mThumbImgPath,
             String mExtension, int mMaxWidth, int mMaxHeight,

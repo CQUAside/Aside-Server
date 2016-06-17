3349ca200b34cb2c894281c76b3d113d2f811062
A Shuai <ls1110924@gmail.com>
2016-06-17 17:40:57 +0800

    refactor imagepath module.

--- src/com/round/aside/server/module/imagepath/IImagePath.java
+++ src/com/round/aside/server/module/imagepath/IImagePath.java
@@ -9,52 +9,62 @@ import com.round.aside.server.module.IModule;
  * 
  */
 public interface IImagePath extends IModule {
-    
+
     /**
      * 获取Logo图文件路径
+     * 
      * @param userId
-     *          用户id
+     *            用户id
      * @param imgId
-     *          图片id
-     * @return
+     *            图片id
+     * @param imgExten
+     *            图片文件扩展名，可以为空
+     * @return Logo图文件路径
      */
-    String logoImgPath(int userId,String imgId, String imgExten);
-    
-    
+    String getLogoImgPath(int userId, String imgId, String imgExten);
+
     /**
      * 获取原图文件路径
+     * 
      * @param userId
-     *          用户id
+     *            用户id
      * @param imgId
-     *          图片id
-     * @return
+     *            图片id
+     * @param imgExten
+     *            图片文件扩展名，可以为空
+     * @return 原图文件路径
      */
-    String originalImgPath(int userId,String imgId, String imgExten);
-    
+    String getOriginalImgPath(int userId, String imgId, String imgExten);
+
     /**
      * 获取缩略图文件路径
+     * 
      * @param userId
-     *          用户id
+     *            用户id
      * @param imgId
-     *          图片id
-     * @return
+     *            图片id
+     * @param imgExten
+     *            图片文件扩展名，可以为空
+     * @return 缩略图文件路径
      */
-    String thumbImgPath(int userId,String imgId, String imgExten);
-    
+    String getThumbImgPath(int userId, String imgId, String imgExten);
+
     /**
      * 获取一个用户对应的原图目录
+     * 
      * @param userId
-     *          用户id
-     * @return
+     *            用户id
+     * @return 用户原图片集所在的目录
      */
-    String userOriginalImgDir(int userId);
-    
+    String getUserOriginalImgDir(int userId);
+
     /**
      * 获取一个用户对应的缩略图目录
+     * 
      * @param uerId
-     *          用户id
-     * @return
+     *            用户id
+     * @return 用户缩略图集所在的目录
      */
-    String userThumbImgDir(int userId);
-    
+    String getUserThumbImgDir(int userId);
+
 }
--- src/com/round/aside/server/module/imagepath/ImagePathImpl.java
+++ src/com/round/aside/server/module/imagepath/ImagePathImpl.java
@@ -3,6 +3,8 @@ package com.round.aside.server.module.imagepath;
 import com.round.aside.server.util.MD5Utils;
 import com.round.aside.server.util.StringUtil;
 
+import static com.round.aside.server.constant.Constants.*;
+
 /**
  * 图片目录模块超级接口的实现类
  * 
@@ -12,43 +14,48 @@ import com.round.aside.server.util.StringUtil;
 public final class ImagePathImpl implements IImagePath {
 
     @Override
-    public String logoImgPath(int userId, String imgId, String imgExten) {        //优化：写成一个函数
-        String str = String.valueOf(userId);
-        String md5path = MD5Utils.encryptionInfoByMd5(str).substring(29, 32);
-        String path = "LogoImages/"+md5path+"/"+userId+"/"+imgId + (StringUtil.isEmpty(imgExten) ? "" : "." + imgExten);
-        return path;
+    public String getLogoImgPath(int userId, String imgId, String imgExten) {
+        return buildImgPath("LogoImg", userId, imgId, imgExten);
     }
 
     @Override
-    public String originalImgPath(int userId, String imgId, String imgExten) {
-        String str = String.valueOf(userId);
-        String md5path = MD5Utils.encryptionInfoByMd5(str).substring(29, 32);
-        String path = "OriginalImages/"+md5path+"/"+userId+"/"+imgId + (StringUtil.isEmpty(imgExten) ? "" : "." + imgExten);
-        return path;
+    public String getOriginalImgPath(int userId, String imgId, String imgExten) {
+        return buildImgPath("OriginalImg", userId, imgId, imgExten);
     }
 
     @Override
-    public String thumbImgPath(int userId, String imgId, String imgExten) {
-        String str = String.valueOf(userId);
-        String md5path = MD5Utils.encryptionInfoByMd5(str).substring(29, 32);
-        String path = "ThumbImages/"+md5path+"/"+userId+"/"+imgId + (StringUtil.isEmpty(imgExten) ? "" : "." + imgExten);
-        return path;
+    public String getThumbImgPath(int userId, String imgId, String imgExten) {
+        return buildImgPath("ThumbImg", userId, imgId, imgExten);
+    }
+
+    private final String buildImgPath(String mImageRootDir, int mUserID,
+            String mImgID, String mImgExten) {
+        StringBuilder mSB = new StringBuilder();
+        mSB.append(ROOT_DIR).append(mImageRootDir).append('/');
+        mSB.append(MD5Utils.encryptionInfoByMd5(mUserID + "").substring(29, 32));
+        mSB.append('/').append(mUserID).append('/').append(mImgID);
+        if (!StringUtil.isEmpty(mImgExten)) {
+            mSB.append('.').append(mImgExten);
+        }
+        return mSB.toString();
     }
 
     @Override
-    public String userOriginalImgDir(int userId) {
-        String str = String.valueOf(userId);
-        String md5path = MD5Utils.encryptionInfoByMd5(str).substring(29, 32);
-        String path = "OriginalImages/"+md5path+"/"+userId;
-        return path;
+    public String getUserOriginalImgDir(int userId) {
+        return buildImgDir("OriginalImg", userId);
     }
 
     @Override
-    public String userThumbImgDir(int userId) {
-        String str = String.valueOf(userId);
-        String md5path = MD5Utils.encryptionInfoByMd5(str).substring(29, 32);
-        String path = "ThumbImages/"+md5path+"/"+userId;
-        return path;
+    public String getUserThumbImgDir(int userId) {
+        return buildImgDir("ThumbImg", userId);
+    }
+
+    private final String buildImgDir(String mImageRootDir, int mUserID) {
+        StringBuilder mSB = new StringBuilder();
+        mSB.append(ROOT_DIR).append(mImageRootDir).append('/');
+        mSB.append(MD5Utils.encryptionInfoByMd5(mUserID + "").substring(29, 32));
+        mSB.append('/').append(mUserID);
+        return mSB.toString();
     }
 
 }
